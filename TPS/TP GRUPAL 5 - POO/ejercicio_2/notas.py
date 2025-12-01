f1bdba1c2cced99bf49001d659d5a094b0577bfa
Renzo <renzochecho416@gmail.com>
2025-12-01 11:52:01 -0300

    unidad

--- TPS/TP GRUPAL 5 - POO/ejercicio_2/notas.py
+++
@@ -1,9 +0,0 @@
-class Nota():
-    def __init__(self, catedra: str, nota: int):
-        if not isinstance(catedra, str):
-            raise TypeError("ERROR: El tipo de dato catedra debe ser un str")
-        self.catedra = catedra
-
-        if not isinstance(nota, float): raise TypeError("ERROR: El tipo de dato nota debe ser un float")
-        if not 1<=nota<=10: raise ValueError("ERROR: El valor de nota debe estar entre 1 y 10")
-        self.nota = nota
